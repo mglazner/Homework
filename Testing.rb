2d9e933c4ed7afc6a77c5aaeb721f5dc471028b7
Megan Glazner <ladyglazner@gmail.com>
2013-12-02 21:00:01 -0800

    Class 12-2-13
    Yo

---
+++ Testing.rb
@@ -0,0 +1,56 @@
+#h={}
+#s="another big long string that has lots of characters"
+#vowels=["a", "e", "i", "o", "u"]
+
+#vowels.each do |c|
+#  h[c]=s.count(c)
+#end
+#puts h
+
+#s.each_char do |c|
+#  if vowels.include?(c)
+#    if h[c]==nil
+#      h[c]=0
+#    end
+#    h[c]+=1
+#  end
+#end
+#puts h
+
+#Using methods
+
+def output_results (result) #result is what we are passing the method 
+  puts result
+end
+
+def create_pair_if_nil (result,c) #"vars" are local and can be reused
+  if result[c]==nil
+     result[c]=0
+  end
+end
+
+def increment (result,c)
+  result[c]+=1
+end
+
+def counting_vowels (result,c,vowels)
+  if vowels.include?(c)
+    create_pair_if_nil(result,c)
+    increment(result,c)
+  end
+end
+
+def main ()
+  h={}
+  s="another big long string that has lots of characters"
+  vowels=["a", "e", "i", "o", "u"]
+  s.each_char do |c|
+    counting_vowels(h,c,vowels)
+  end
+  output_results(h)
+end
+
+## So this becomes our entire "function"
+main()
+##
+
--- jason.rb
+++ jason.rb
@@ -16,6 +16,7 @@
 
  megan=[10,9,8,7,6,5,4,3,2,1]
 # => [10,9,8,7,6,5,4,3,2,1]	# building the array
+puts megan
 
 
  megan.sort
