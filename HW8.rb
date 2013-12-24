d531f0a37b4efd378b22b780cf8e3989e84bd232
Jason R. Clark <jclark@newrelic.com>
2013-12-23 20:55:42 -0800

    HW8

---
+++ HW8.rb
@@ -0,0 +1,33 @@
+# Update this program to keep a running tally of the same stats we were
+# puts'ing in babylibrary v1.
+#
+# > A string here
+# > Another string here
+# > exit
+#   A
+#   string
+#   here
+#   Another
+#   string
+#   here
+#
+# EXTRA CREDIT! Rather than tallying up the strings, instead process each
+# string and hold onto another form of data until the end, and print output
+# from that.
+
+require_relative "babylibrary"
+
+def main(s)
+  separate_line(s)
+  upcase(s)
+  replace_spaces(s)
+  count_words(s)
+  count_punct(s)
+  sort(s)
+end
+
+loop do
+  s=gets.chomp
+  break if s=="exit"
+  main(s)
+end
