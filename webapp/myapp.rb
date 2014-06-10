da3f7e9c4205a0f4459322d54b6aa89d0c702dfe
Megan Glazner <ladyglazner@gmail.com>
2014-06-09 21:15:02 -0700

    Basic Templating
    Our first forays into ERB and templating

--- webapp/myapp.rb
+++ webapp/myapp.rb
@@ -1,20 +1,28 @@
 
 class Myapp
 
+SECRETID={"Tony Stark"=>"Ironman","Steve Rogers"=>"Captain America","Bruce Banner"=>"The Hulk","The Avengers"=>"The Avengers"}
+
   def call(env)
     p env
+	template=ERB.new(File.read("template.erb"))
     if env["PATH_INFO"]=="/"
       [404,{"Content-Type"=>"text"},["Please try again."]]
     elsif File.exists?("."+(env["PATH_INFO"]))
-      result=File.read("."+(env["PATH_INFO"]))
-      success(result)       
+      quotetext=File.read("."+(env["PATH_INFO"]))
+	  heroname=env["PATH_INFO"]
+	  heroname.gsub!('_',' ')
+	  heroname.gsub!('/','')
+	  heroname.gsub!('.txt','')
+	  identity=SECRETID[heroname]
+	  success(template.result(binding))       
     else 
       [404,{"Content-Type"=>"text"},["Please try again."]]
     end
   end
   
-  def success(name)
-    [200,{"Content-Type"=>"text"},[name]] 
+  def success(content)
+    [200,{"Content-Type"=>"text"},[content]] 
   end
 
 end
