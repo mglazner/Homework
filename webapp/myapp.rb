b3009ba792c323f61f943c275e2aac4c60591c43
Megan Glazner <ladyglazner@gmail.com>
2014-10-17 07:24:55 -0700

    Ketchup--html
    I have been neglecting this recently, so here is a dump of everything,
    which is a really poor tag for this push.

--- webapp/myapp.rb
+++ webapp/myapp.rb
@@ -9,23 +9,37 @@ SECRETID=
   "The Avengers"=>"The Avengers",
   "Natasha Romanoff"=>"The Black Widow",
   "Clint Barton"=>"Hawkeye",
-  "Thor Odinsson"=>"Thor, God of Thunder"}
+  "Thor Odinson"=>"Thor, God of Thunder"}
+  
+HEADSHOTS=
+  {"Tony Stark"=>"http://media.tumblr.com/2f5a17eda02e948ddaf1c8247929ce2d/tumblr_inline_n5m30dgncd1sy66lj.gif",
+  "Steve Rogers"=>"http://thefilmexperience.net/storage/2014/captainamerica-newcostume.jpg?__SQUARESPACE_CACHEVERSION=1396597020834",
+  "Bruce Banner"=>"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRR_Fxff5pzYuTg6_ZYVA06X1XsGtqCFbbAA25zFBMLvoulfrJz",
+  "The Avengers"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX2U2QppPwQMo11uwUxUrrXG_R9SbTamGaUSxNIvLK0E2-QdkcGg",
+  "Natasha Romanoff"=>"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTSfKqSe6_vXhWD7QMf15TSv2w2fOZcrG4-e2wKVK2aP2RwnPTQ",
+  "Clint Barton"=>"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTZOc-O1tlTEc2juYe0pVEhM2aRUSEaq7a_7Kk0EGN5hf-nNEya",
+  "Thor Odinson"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRk43atCq1Vj2Ouj1R_zMeV8MQ6yPeCBtQNDT33s_Y4-MZx4uS"}
 
   def call(env)
     p env
-    template=ERB.new(File.read("template.erb"))
-    if env["PATH_INFO"]=="/"
-      [404,{"Content-Type"=>"text"},["Please try again."]]
-    elsif File.exists?("."+(env["PATH_INFO"]))
-      quotetext=File.read("."+(env["PATH_INFO"]))
-      heroname=env["PATH_INFO"]
-      heroname.gsub!('_',' ')
-      heroname.gsub!('/','')
-      heroname.gsub!('.txt','')
-      identity=SECRETID[heroname]
-      success(template.result(binding))       
-    else 
-      [404,{"Content-Type"=>"text"},["Please try again."]]
+    if env["REQUEST_METHOD"]=="GET"   
+      template=ERB.new(File.read("template.erb"))
+      if env["PATH_INFO"]=="/"
+        [404,{"Content-Type"=>"text"},["Please try again."]]
+      elsif File.exists?("."+(env["PATH_INFO"]))
+        quotetext=File.read("."+(env["PATH_INFO"]))
+        heroname=env["PATH_INFO"]
+        heroname.gsub!('_',' ')
+        heroname.gsub!('/','')
+        heroname.gsub!('.txt','')
+        identity=SECRETID[heroname]
+        picture=HEADSHOTS[heroname]
+        success(template.result(binding))       
+      else 
+        [404,{"Content-Type"=>"text"},["Please try again."]]
+      end
+    elsif env["REQUEST_METHOD"]=="POST"
+      success(env["rack.input"].read)
     end
   end
   
